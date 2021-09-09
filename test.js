0e639e9552bf84f677aa7cc58e188eaaf8169c89
ScorpionSquad <steamgozuk@gmail.com>
2021-09-09 20:53:49 +0200

    cum

--- test.js
+++
@@ -1,5 +0,0 @@
-const ligma = document.getElementById("ligma");
-ligma.addEventListener("click", ligmaballssugondeseligondesenutsbitch)
-function ligmaballssugondeseligondesenutsbitch() {
-    document.getElementById("ligma").style.color = "red";
-}
